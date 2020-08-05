e3eb20ec09ed9d956d06d5ddf000cd69d01ca2f6
salizw27 <salizwamayekiso27@gmail.com>
2020-08-05 12:54:29 +0200

    made changes to my greet_xola.js file

--- greet_xola.js
+++ greet_xola.js
@@ -2,5 +2,18 @@ const chalk = require('chalk');
 
 const greet = require('./greet');
 
-const styledMessage = chalk.bgGreen.black(greet('Xola'));
-console.log(styledMessage)
+var figlet = require('figlet');
+ 
+figlet((greet('Xola')), function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    
+    const styledMessage = chalk.bgGreen.black(data);
+    console.log(styledMessage)
+    
+
+});
+
